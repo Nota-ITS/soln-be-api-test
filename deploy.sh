751a874279f7a14e0105cd6c84213ca72d24368a
yoonnota <hyejeong.yun@nota.ai>
2025-03-24 13:14:20 +0900

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
 # 설정
-REPO_DIR="/home/itsdev/workspace/soln-be-api-test"  # Git 저장소 경로
-APP_DIR="/home/itsdev/workspace/soln-be-api-test/my_flask_app"  # Git 저장소 경로
-IMAGE_NAME="my_flask_app"      # Docker 이미지 이름
-CONTAINER_NAME="my_flask_app"  # Docker 컨테이너 이름
-SLACK_WEBHOOK_URL="https://hooks.slack.com/services/your/webhook/url"  # Slack Webhook URL
+# .env 파일 경로를 인자로 받음
+ENV_FILE=$1
+
+# .env 파일 로드
+if [ -z "$ENV_FILE" ]; then
+    echo "Usage: $0 path/to/.env"
+    exit 1
+fi
+
+if [ -f "$ENV_FILE" ]; then
+    export $(cat "$ENV_FILE" | xargs)
+else
+    echo ".env file not found at $ENV_FILE!"
+    exit 1
+fi
 
 # Slack 알림 함수
 send_slack_notification() {
-    # local message=$1
-    # curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$message\"}" $SLACK_WEBHOOK_URL
     local message=$1
-    echo $message
+    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$message\"}" $SLACK_WEBHOOK_URL
+    # local message=$1
+    # echo $message
 }
 
 # 오류 처리 함수
@@ -28,10 +38,12 @@ cd $REPO_DIR || handle_error "Failed to change directory to $REPO_DIR"
 git checkout develop || handle_error "Failed to checkout develop branch"
 git pull origin develop || handle_error "Failed to pull from develop branch"
 
+# 현재 커밋 해시 가져오기
+commit_hash=$(git rev-parse HEAD)
+
 # Docker 이미지 생성
 echo "Building Docker image..."
-cd $APP_DIR || handle_error "Failed to change app directory to $APP_DIR"
-docker build -t $IMAGE_NAME . || handle_error "Failed to build Docker image"
+docker build -t $IMAGE_NAME $DOCKERFILE_PATH || handle_error "Failed to build Docker image"
 
 # 기존 컨테이너 중지 및 삭제
 echo "Stopping and removing existing container..."
@@ -43,6 +55,6 @@ echo "Starting new container..."
 docker run -d -p 5000:5000 --name $CONTAINER_NAME $IMAGE_NAME || handle_error "Failed to start Docker container"
 
 # 성공 알림 전송
-send_slack_notification "Deployment completed successfully!"
+send_slack_notification "Deployment completed successfully!\nContainer: $CONTAINER_NAME\nCommit: $commit_hash"
 
 echo "Done."
